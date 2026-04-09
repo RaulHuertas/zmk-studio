405d252325860385f11ba23a27375ad97dbc7a4b
RaulHuertas <rax20037@gmail.com>
2026-04-08 20:16:41 -0500

    fixes in the visualization

--- src/keyboard/Keymap.tsx
+++ src/keyboard/Keymap.tsx
@@ -51,33 +51,9 @@ export const Keymap = ({
       };
     }
 
-    console.log(      behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]
-        ?.displayName == "To Layer"
-    )
-    if (
-      behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]
-        ?.displayName == "To Layer"
-    ) {
-      //console.log("Found a to layer behavior at position " + i);
-      //console.log(behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]?.metadata)
-      //console.log(keymap.layers[selectedLayerIndex].bindings[i].param1);
-    }
-    if (
-      behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]
-        ?.displayName == "Momentary Layer"
-    ) {
-      //console.log("Found a mo layer behavior at position " + i);
-      //console.log(behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]?.metadata)
-      //console.log(keymap.layers[selectedLayerIndex].bindings[i].param1);
-    }
-    if (
-      behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]
-        ?.displayName == "Key Press"
-    ) {
-      //console.log("Found a keypress behavior at position " + i);
-      //console.log(behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]?.metadata)
-      //console.log(keymap.layers[selectedLayerIndex].bindings[i].param1);
-    }
+    console.log(      behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]?.displayName    )
+    console.log(      keymap.layers[selectedLayerIndex].bindings[i].param1    )
+    console.log(      keymap.layers[selectedLayerIndex].bindings[i].param2    )
     const header =
       behaviors[keymap.layers[selectedLayerIndex].bindings[i].behaviorId]
         ?.displayName || "Unknown";
